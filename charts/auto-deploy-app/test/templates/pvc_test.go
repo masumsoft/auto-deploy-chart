a3237745bb07d624c9d5e1abf6bb32adeb97b0fb
Masum <masum@surecash.net>
2025-04-17 12:33:37 +0600

    merged upstream changes to upgrade to 2.80.1

--- charts/auto-deploy-app/test/templates/pvc_test.go
+++ charts/auto-deploy-app/test/templates/pvc_test.go
@@ -16,6 +16,7 @@ func TestPvcTemplate_Single(t *testing.T) {
 	templates := []string{"templates/pvc.yaml"}
 	releaseName := "test"
 	customStorageClassName := "MyStorageClass"
+	customVolumeName := "MyVolumeName"
 	expectedLabels := map[string]string{
 		"app":                          releaseName,
 		"chart":                        chartName,
@@ -53,6 +54,7 @@ func TestPvcTemplate_Single(t *testing.T) {
 				"persistence.volumes[0].claim.accessMode":   "ReadOnlyMany",
 				"persistence.volumes[0].claim.size":         "20Gi",
 				"persistence.volumes[0].claim.storageClass": customStorageClassName,
+				"persistence.volumes[0].claim.volumeName":   customVolumeName,
 				"persistence.volumes[0].mount.path":         "/log",
 			},
 			expectedMeta: metav1.ObjectMeta{Name: "test-auto-deploy-log-dir", Labels: expectedLabels},
@@ -60,6 +62,7 @@ func TestPvcTemplate_Single(t *testing.T) {
 				AccessModes:      [](coreV1.PersistentVolumeAccessMode){coreV1.ReadOnlyMany},
 				Resources:        coreV1.ResourceRequirements{Requests: coreV1.ResourceList{"storage": resource.MustParse("20Gi")}},
 				StorageClassName: &customStorageClassName,
+				VolumeName:       customVolumeName,
 			},
 		},
 		{
@@ -71,11 +74,10 @@ func TestPvcTemplate_Single(t *testing.T) {
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			output, ret := renderTemplate(t, tc.values, releaseName, templates, tc.expectedErrorRegexp)
-
-			if ret == false {
-				return
+			opts := &helm.Options{
+				SetValues: tc.values,
 			}
+			output := mustRenderTemplate(t, opts, releaseName, templates, tc.expectedErrorRegexp)
 
 			pvc := new(coreV1.PersistentVolumeClaim)
 			helm.UnmarshalK8SYaml(t, output, pvc)
@@ -84,6 +86,7 @@ func TestPvcTemplate_Single(t *testing.T) {
 			require.Equal(t, tc.expectedPVC.AccessModes, pvc.Spec.AccessModes)
 			require.Equal(t, tc.expectedPVC.Resources.Requests["storage"], pvc.Spec.Resources.Requests["storage"])
 			require.Equal(t, tc.expectedPVC.StorageClassName, pvc.Spec.StorageClassName)
+			require.Equal(t, tc.expectedPVC.VolumeName, pvc.Spec.VolumeName)
 		})
 	}
 }
@@ -92,6 +95,7 @@ func TestPvcTemplate_Multiple(t *testing.T) {
 	templates := []string{"templates/pvc.yaml"}
 	releaseName := "test"
 	customStorageClassName := "MyStorageClass"
+	customVolumeName := "MyVolumeName"
 	expectedLabels := map[string]string{
 		"app":                          releaseName,
 		"chart":                        chartName,
@@ -124,6 +128,7 @@ func TestPvcTemplate_Multiple(t *testing.T) {
 				"persistence.volumes[1].claim.accessMode":   "ReadOnlyMany",
 				"persistence.volumes[1].claim.size":         "20Gi",
 				"persistence.volumes[1].claim.storageClass": customStorageClassName,
+				"persistence.volumes[1].claim.volumeName":   customVolumeName,
 				"persistence.volumes[1].mount.path":         "/log",
 			},
 			expectedMetas: []metav1.ObjectMeta{
@@ -139,6 +144,7 @@ func TestPvcTemplate_Multiple(t *testing.T) {
 					AccessModes:      [](coreV1.PersistentVolumeAccessMode){coreV1.ReadOnlyMany},
 					Resources:        coreV1.ResourceRequirements{Requests: coreV1.ResourceList{"storage": resource.MustParse("20Gi")}},
 					StorageClassName: &customStorageClassName,
+					VolumeName:       customVolumeName,
 				},
 			},
 		},
@@ -146,11 +152,10 @@ func TestPvcTemplate_Multiple(t *testing.T) {
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			output, ret := renderTemplate(t, tc.values, releaseName, templates, tc.expectedErrorRegexp)
-
-			if ret == false {
-				return
+			opts := &helm.Options{
+				SetValues: tc.values,
 			}
+			output := mustRenderTemplate(t, opts, releaseName, templates, tc.expectedErrorRegexp)
 
 			pvcList := strings.Split(output, "---")
 			for i, pvcOutput := range pvcList[1:] {
@@ -160,6 +165,7 @@ func TestPvcTemplate_Multiple(t *testing.T) {
 				require.Equal(t, tc.expectedPVCs[i].AccessModes, pvc.Spec.AccessModes)
 				require.Equal(t, tc.expectedPVCs[i].Resources.Requests["storage"], pvc.Spec.Resources.Requests["storage"])
 				require.Equal(t, tc.expectedPVCs[i].StorageClassName, pvc.Spec.StorageClassName)
+				require.Equal(t, tc.expectedPVCs[i].VolumeName, pvc.Spec.VolumeName)
 			}
 		})
 	}
